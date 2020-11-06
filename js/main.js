f18b31238d5d0fdfc26339539b64a664e15e6077
EvgeniyLazoriv <EvgeniyLazoriv@yandex.com>
2020-11-06 09:08:10 +0200

    matching

--- js/main.js
+++ js/main.js
@@ -7,4 +7,60 @@ $(document).ready(function(){
 		$('.header__phones').toggleClass('active');
 		$('.header__menu, .header__burger').removeClass('active');
 	});
+
+	/***  Section Colors start  ***/
+
+	$( ".text__radiator" ).click( function() {
+		if ($("#romb-one").css('transform') == 'none') {
+		  $("#romb-one").css({'transform': 'rotate(-180deg)'});
+		} else {
+		  $("#romb-one").css({'transform': ''});
+		};
+	});
+
+	$( ".text__towel" ).click( function() {
+		if ($("#romb-two").css('transform') == 'none') {
+		  $("#romb-two").css({'transform': 'rotate(-180deg)'});
+		} else {
+		  $("#romb-two").css({'transform': ''});
+		};
+	});
+
+	$( ".text__multicolored" ).click( function() {
+		if ($("#romb-three").css('transform') == 'none') {
+		  $("#romb-three").css({'transform': 'rotate(-180deg)'});
+		} else {
+		  $("#romb-three").css({'transform': ''});
+		};
+	});
+
+	$(".colors__text--one").click(function(){
+		$('.radiators').hide();
+		$('#rad1').show();		
+	});
+
+	$(".colors__text--two").click(function(){
+		$('.radiators').hide();
+		$('#rad2').show();		
+	});
+
+	$(".colors__text--three").click(function(){
+		$('.radiators').hide();
+		$('#rad3').show();		
+	});
+
+	$(".towel-1").click(function(){
+		$('.towels').hide();
+		$('#towel1').show();		
+	});
+
+	$(".towel-2").click(function(){
+		$('.towels').hide();
+		$('#towel2').show();		
+	});
+
+	/***  Section Colors finish  ***/
+	
+	
+
 });
