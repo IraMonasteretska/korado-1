f9011807be30253b84319ccb9320fab30f1c241b
EvgeniyLazoriv <EvgeniyLazoriv@yandex.com>
2020-11-16 15:05:50 +0200

    15:05

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-	$('.header__burger').click(function(event) {
+	$('.header__burger').click(function() {
 		$('.header__burger,.header__menu').toggleClass('active');
 		$('.header__phones').removeClass('active');
 	});
-	$('.header__btn').click(function(event) {
+	$('.header__btn').click(function() {
 		$('.header__phones').toggleClass('active');
 		$('.header__menu, .header__burger').removeClass('active');
 	});
